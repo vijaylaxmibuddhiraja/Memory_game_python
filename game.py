641212a61b505e7effc6bcad9191a6b9198eff26
Vijaylaxmi Buddhiraja <vijaylaxmi.buddhiraja@gmail.com>
2025-12-18 20:37:25 +0100

    levels can be selected and are working but the wait timing is not shown

--- game.py
+++ game.py
@@ -1,5 +1,4 @@
 import os
-from pdb import main
 import random 
 import time
 
@@ -9,23 +8,55 @@ def clear_screen():
 
 def display_intro():
     # Display the welcome message
-    print("=====================================")
-    print (" Welcome to the memory game!")
-    print("=====================================")
+    while True:   
+        clear_screen()
+        print("=====================================")
+        print (" Welcome to the memory game!")
+        print("=====================================")
 
-    # Select levels
-    print("\nSelect a difficulty level:")
-    print("A. Beginner")
-    print("B. Intermediate")
-    print("C. Expert")
-    print("D. Quit")
+        # Select levels
+        print("\nSelect a difficulty level:")
+        print("A. Beginner")
+        print("B. Intermediate")
+        print("C. Expert")
+        print("D. Quit")
 
-    choice = input("\nEnter your choice: ")
+        choice = input("\nEnter your choice: ")
 
-    if choice == "A" or choice.lower() == "beginner":
-        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        # QUITTING the game 
+        if choice == "D" or choice.lower() == "quit":
+             confirm = input("Are you sure you want to quit? (Y/N): ").lower()
+             if confirm == "y":
+                 print("Thank you for playing! Goodbye!")
+                 break
+             else:
+                 continue
+             
 
-        original_sequence = random.sample(alphabet, 5)
+        # BEGINNER LEVEL
+        if choice == "A" or choice.lower() == "beginner":
+            alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            count = 5
+            wait_time = 5
+
+        # INTERMEDIATE LEVEL
+        elif choice == "B" or choice.lower() == "intermediate":
+            alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            count = 7
+            wait_time = 7  
+
+        # EXPERT LEVEL
+        elif choice == "C" or choice.lower() == "expert":
+            alphabet = "DRAGON", "MAMOTH", "PHOENIX", "MOUNTAIN", "FOREST", "ISLAND",
+            "RIVER", "OCEAN", "DESERT", "CANYON", "VALLEY", "GLACIER"
+            count = 5
+            wait_time = 10     
+        else:
+            print("Invalid choice. Please try again.")
+            time.sleep(2)
+            continue
+
+        original_sequence = random.sample(alphabet, count)
 
         clear_screen()
 
@@ -33,8 +64,8 @@ def display_intro():
         print(" ".join(original_sequence))
 
 
-        print("\nYou have 5 seconds to memorize the sequence.")
-        time.sleep(5)
+        print("\nYou have {wait_time} seconds to memorize the sequence.")
+        time.sleep(wait_time)
         clear_screen()
 
         #make the shuffling
@@ -43,17 +74,18 @@ def display_intro():
 
         #Ask the user to input the sequence
         user_input = input("Enter the correct sequence (with spaces): ").upper().split()
+        
         if user_input == original_sequence:
             print(" You entered the correct sequence.")
         else:
             print("Oops! Wrong answer.")
             print(f"\nThe correct sequence was: { ' '.join(original_sequence)}")
+
+        input("\nPress Enter to return to main menu..")       
         #print("Time's up!Please enter the sequence.")
 
 
-    #if choice == "D" or choice.lower() == "quit":
-      #  print("Thanks for playing! Goodbye!")
-      #  return
+    
     
     #to test if it works
     #print(f"You selected {choice} level.")  
