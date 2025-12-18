563861db49c32f06367d9cbe89a4fe1bb6439625
Vijaylaxmi Buddhiraja <vijaylaxmi.buddhiraja@gmail.com>
2025-12-18 21:23:02 +0100

    removed some codes but level options are not functioning

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ def display_intro():
     while True:   
         clear_screen()
         print("=====================================")
-        print (" Welcome to the memory game!")
+        print (" Welcome to the Memory Game!")
         print("=====================================")
 
         # Select levels
@@ -21,72 +21,70 @@ def display_intro():
         print("C. Expert")
         print("D. Quit")
 
-        choice = input("\nEnter your choice: ")
+        choice = input("\nEnter your choice: ").upper()
 
         # QUITTING the game 
-        if choice == "D" or choice.lower() == "quit":
-             confirm = input("Are you sure you want to quit? (Y/N): ").lower()
-             if confirm == "y":
+        if choice == "D" or choice == "":
+             confirm = input("Are you sure you want to quit? (Y/N): ").upper()
+             if confirm == "Y":
                  print("Thank you for playing! Goodbye!")
                  break
              else:
                  continue
              
-
+        # START THE GAME
+        #Set variables based on level
         # BEGINNER LEVEL
         if choice == "A" or choice.lower() == "beginner":
-            alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            letter_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
             count = 5
             wait_time = 5
 
         # INTERMEDIATE LEVEL
         elif choice == "B" or choice.lower() == "intermediate":
-            alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            letter_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
             count = 7
             wait_time = 7  
 
         # EXPERT LEVEL
         elif choice == "C" or choice.lower() == "expert":
-            alphabet = "DRAGON", "MAMOTH", "PHOENIX", "MOUNTAIN", "FOREST", "ISLAND",
-            "RIVER", "OCEAN", "DESERT", "CANYON", "VALLEY", "GLACIER"
-            count = 5
-            wait_time = 10     
+             letter_list = "DRAGON", "MAMOTH", "PHOENIX", "MOUNTAIN", "FOREST", "ISLAND",
+             "RIVER", "OCEAN", "DESERT", "CANYON", "VALLEY", "GLACIER"
+             count = 5
+             wait_time = 10     
         else:
             print("Invalid choice. Please try again.")
             time.sleep(2)
-            continue
+            
 
-        original_sequence = random.sample(alphabet, count)
-
-        clear_screen()
-
-        print("\nMemorize this sequence:")
-        print(" ".join(original_sequence))
-
-
-        print("\nYou have {wait_time} seconds to memorize the sequence.")
-        time.sleep(wait_time)
-        clear_screen()
-
-        #make the shuffling
-        mixed_sequence = list(original_sequence)
-        random.shuffle(mixed_sequence)
-
-        #Ask the user to input the sequence
-        user_input = input("Enter the correct sequence (with spaces): ").upper().split()
+        # Generate random sequence
         
-        if user_input == original_sequence:
-            print(" You entered the correct sequence.")
-        else:
-            print("Oops! Wrong answer.")
-            print(f"\nThe correct sequence was: { ' '.join(original_sequence)}")
+            original_sequence = random.sample(letter_list, count)
+            clear_screen()
 
-        input("\nPress Enter to return to main menu..")       
-        #print("Time's up!Please enter the sequence.")
+            print("\nMemorize this sequence:")
+            print(" ".join(original_sequence))
 
+            print(f"\nYou have {wait_time} seconds to memorize the sequence.")
+            time.sleep(wait_time)
+            clear_screen()
 
-    
-    
+            #make the shuffling
+            mixed_sequence = list(original_sequence)
+            random.shuffle(mixed_sequence)
+
+            #Ask the user to input the sequence
+            user_input = input("Enter the correct sequence (with spaces): ").upper().split()
+        
+            if user_input == original_sequence:
+                print(" You entered the correct sequence.")
+            else:
+                print("Oops! Wrong answer.")
+                print(f"\nThe correct sequence was: { ' '.join(original_sequence)}")
+
+            time.sleep(3)
+     
+            
     #to test if it works
     #print(f"You selected {choice} level.")  
     
